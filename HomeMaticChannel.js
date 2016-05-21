0c310559d21eddde6d8c8fb966df11099914c1f0
Carsten Duvenhorst <carsten@duvenhorst.net>
2016-05-21 12:52:51 +0200

    Added ON_TIME Datapoint and custom property for SWITCH Channel

--- HomeMaticChannel.js
+++ HomeMaticChannel.js
@@ -93,6 +93,25 @@ function HomeMaticGenericChannel(log,platform, id ,name, type ,adress,special, c
     cc.eventEnabled = true;
 
     this.remoteGetValue("STATE");
+      
+      var onTimeProperties = {
+          format: Characteristic.Formats.FLOAT,
+          unit: Characteristic.Units.SECONDS,
+          minValue: 0,
+          maxValue: 3600.0, // normally defined as 85825945.6 but that`s in Hesperus inconvenient and unusable
+          minStep: 1,
+          perms: [Characteristic.Perms.WRITE]
+        };
+
+      var on_time = new Characteristic("OnTime","CEA288AC-EAC5-447A-A2DD-D684E4517440", onTimeProperties)
+          .on('set', function(value, callback) {
+            that.command("set","ON_TIME" , value);
+            callback();
+          }.bind(this));
+
+        on_time.eventEnabled = true;
+
+        lightbulb.addCharacteristic(on_time);
 
     break;
 
