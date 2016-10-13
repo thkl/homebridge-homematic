ec56c52e0250cb142c3768e599fc7888f039cd00
Thomas Kluge <th.kluge@me.com>
2016-10-13 19:14:34 +0200

    Added CuxD Thermometer with Humidity Sensor

---
+++ ChannelServices/HomeMaticHomeKitCuxDThermostatService.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var HomeKitGenericService = require('./HomeKitGenericService.js').HomeKitGenericService;
+var util = require("util");
+
+
+function HomeMaticHomeKitCuxDThermostatService(log,platform, id ,name, type ,adress,special, cfg, Service, Characteristic) {
+    HomeMaticHomeKitCuxDThermostatService.super_.apply(this, arguments);
+}
+
+util.inherits(HomeMaticHomeKitCuxDThermostatService, HomeKitGenericService);
+
+
+HomeMaticHomeKitCuxDThermostatService.prototype.createDeviceService = function(Service, Characteristic) {
+
+   var that = this;
+   this.usecache = false;
+   var thermo = new Service.TemperatureSensor(this.name);
+   this.services.push(thermo);
+
+   var cctemp = thermo.getCharacteristic(Characteristic.CurrentTemperature)
+   .setProps({ minValue: -100 })
+    .on('get', function(callback) {
+      this.remoteGetValue("TEMPERATURE",function(value){
+       if (callback) callback(null,value);
+      });
+    }.bind(this));
+
+   this.currentStateCharacteristic["TEMPERATURE"] = cctemp;
+   cctemp.eventEnabled = true;
+   
+   var cchum = thermo.addCharacteristic(Characteristic.CurrentRelativeHumidity)
+   .on('get', function(callback) {
+      this.remoteGetValue("HUMIDITY",function(value){
+       if (callback) callback(null,value);
+      });
+    }.bind(this));
+
+    this.currentStateCharacteristic["HUMIDITY"] = cchum;
+    cchum.eventEnabled = true;
+
+   this.remoteGetValue("TEMPERATURE");
+   this.remoteGetValue("HUMIDITY");
+
+}
+
+
+
+module.exports = HomeMaticHomeKitCuxDThermostatService; 
