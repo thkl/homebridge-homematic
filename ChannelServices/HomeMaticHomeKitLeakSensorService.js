22e805d77060c1aa1aa016136bc8dfc815788289
Thomas Kluge <th.kluge@me.com>
2016-11-18 15:22:28 +0100

    will it blend (aehm leak)

---
+++ ChannelServices/HomeMaticHomeKitLeakSensorService.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var HomeKitGenericService = require('./HomeKitGenericService.js').HomeKitGenericService;
+var util = require("util");
+
+
+function HomeMaticHomeKitLeakSensorService(log,platform, id ,name, type ,adress,special, cfg, Service, Characteristic) {
+    HomeMaticHomeKitLeakSensorService.super_.apply(this, arguments);
+}
+
+util.inherits(HomeMaticHomeKitLeakSensorService, HomeKitGenericService);
+
+
+HomeMaticHomeKitLeakSensorService.prototype.createDeviceService = function(Service, Characteristic) {
+
+    var that = this;
+    var leak_sensor = new Service["LeakSensor"](this.name);
+    var state = leak_sensor.getCharacteristic(Characteristic.LeakDetected)
+	.on('get', function(callback) {
+      that.query("STATE",function(value){
+       if (callback) callback(null,value);
+      });
+    }.bind(this));
+
+    this.currentStateCharacteristic["STATE"] = state;
+    state.eventEnabled = true;
+    this.services.push(leak_sensor);
+    this.remoteGetValue("STATE");
+
+}
+
+
+
+module.exports = HomeMaticHomeKitLeakSensorService; 
