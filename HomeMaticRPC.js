e70b8de6f9aec438735a72bcb55341c208b6998f
Thomas Kluge <th.kluge@me.com>
2016-12-18 10:45:35 +0100

    BigAss Warning if on of the local RPC Server ports are in use ...

--- HomeMaticRPC.js
+++ HomeMaticRPC.js
@@ -8,7 +8,7 @@ var HomeMaticRPC = function (log, ccuip,port,system,platform) {
   
   this.log = log;
   
-  this.log("init RPC");
+  this.log.info("init RPC for %",system);
   this.system = system;
   this.ccuip = ccuip;
   this.platform = platform;
@@ -60,43 +60,45 @@ HomeMaticRPC.prototype.init = function() {
     }
 
     this.localIP = ip;
-    this.log("Local IP: " + this.localIP);
+    
+    this.isPortTaken(this.listeningPort,function(error,inUse){
 
-    this.server = binrpc.createServer({
-      host: this.localIP,
-      port: this.listeningPort
-    });
+     if (inUse == false) {
+         that.server = binrpc.createServer({
+	      host: that.localIP,
+    	  port: that.listeningPort
+         });
+         
+     that.server.on("NotFound", function(method, params) {
+      that.log.debug("Method %s does not exist. - %s",method, JSON.stringify(params));
+     });
 
-    this.server.on("NotFound", function(method, params) {
-      debug("Method " + method + " does not exist. - " + JSON.stringify(params));
-    });
-
-    this.server.on("system.listMethods", function(err, params, callback) {
-      debug("Method call params for 'system.listMethods': " +  JSON.stringify(params));
+    that.server.on("system.listMethods", function(err, params, callback) {
+      that.log.debug("Method call params for 'system.listMethods': %s" ,  JSON.stringify(params));
       callback(null, ["event","system.listMethods", "system.multicall"]);
     });
     
-    this.server.on("listDevices", function(err, params, callback) {
-      debug('rpc <- listDevices on '  + that.interface + ' - Zero Reply');
+    that.server.on("listDevices", function(err, params, callback) {
+      that.log.debug('rpc <- listDevices on %s - Zero Reply',that.interface);
       callback(null,[]);
     });
 
 
-	this.server.on("newDevices", function(err, params, callback) {
-      debug('rpc <- newDevices on '  + that.interface + ' nobody is interested in newdevices ... ');
+	that.server.on("newDevices", function(err, params, callback) {
+      that.log.debug('rpc <- newDevices on %s nobody is interested in newdevices ... ',that.interface);
       // we are not intrested in new devices cause we will fetch them at launch
       callback(null,[]);
     });
 
 
-	this.server.on("event", function(err, params, callback) {
- 	  debug('rpc <- event  on '  + this.interface );
+	that.server.on("event", function(err, params, callback) {
+ 	  that.log.debug('rpc <- event  on %s'  , this.interface );
  	  that.lastMessage = Math.floor((new Date()).getTime() / 1000);
       var channel = that.interface + params[1];
       var datapoint = params[2];
       var value = params[3];
-      debug("Ok here is the Event" + JSON.stringify(params));
-      debug("RPC single event for %s %s with value %s",channel,datapoint,value);
+      that.log.debug("Ok here is the Event" + JSON.stringify(params));
+      that.log.debug("RPC single event for %s %s with value %s",channel,datapoint,value);
 			 
       that.platform.foundAccessories.map(function(accessory) {
        if ((accessory.adress == channel) || ((accessory.cadress != undefined) && (accessory.cadress == channel))) {
@@ -106,8 +108,8 @@ HomeMaticRPC.prototype.init = function() {
       callback(null,[]);
 	});
     
-    this.server.on("system.multicall", function(err, params, callback) {
- 	  debug('rpc <- system.multicall on '  + that.interface);
+    that.server.on("system.multicall", function(err, params, callback) {
+ 	  that.log.debug('rpc <- system.multicall on %s'  , that.interface);
       that.lastMessage = Math.floor((new Date()).getTime() / 1000);
       
       params.map(function(events) {
@@ -119,7 +121,7 @@ HomeMaticRPC.prototype.init = function() {
               var channel = that.interface + params[1];
               var datapoint = params[2];
               var value = params[3];
-          	  debug("RPC event for %s %s with value %s",channel,datapoint,value);
+          	  that.log.debug("RPC event for %s %s with value %s",channel,datapoint,value);
 			 
               that.platform.foundAccessories.map(function(accessory) {
 	             var deviceAdress = channel.slice(0,channel.indexOf(":"));
@@ -140,9 +142,18 @@ HomeMaticRPC.prototype.init = function() {
       callback(null);
     });
 
-    this.log("XML-RPC server for interface " + this.interface + "is listening on port " + this.listeningPort);
-    this.connect();
-
+    that.log.info("XML-RPC server for interface %s is listening on port %s.",that.interface, that.listeningPort);
+    that.connect();
+         
+     } else {
+       that.log.error("****************************************************************************************************************************");
+       that.log.error("*  Sorry the local port %s on your system is in use. Please make sure, that no other instance of this plugin is running.",that.listeningPort);
+       that.log.error("*  you may change the initial port with the config setting for local_port in your config.json ");
+       that.log.error("*  giving up ... the homematic plugin is not able to listen for ccu events on %s until you fix this. ",that.interface);
+       that.log.error("****************************************************************************************************************************");
+     }
+    
+    });
   }
 
   HomeMaticRPC.prototype.getIPAddress = function() {
@@ -162,15 +173,15 @@ HomeMaticRPC.prototype.init = function() {
 
     var that = this;
     if (this.client === undefined) {
-      that.log("Returning cause client is invalid");
+      this.log.debug("Returning cause client is invalid");
       return;
     }
     if (channel.indexOf(that.interface) > -1)  {
       channel = channel.substr(that.interface.length);
 
-	  debug("RPC getValue Call for %s %s",channel,datapoint);
+	  this.log.debug("RPC getValue Call for %s %s",channel,datapoint);
 	  this.client.methodCall("getValue", [channel, datapoint], function(error, value) {
-    	debug("RPC getValue (%s %s) Response %s Errors: %s",channel,datapoint, JSON.stringify(value),error);
+    	that.log.debug("RPC getValue (%s %s) Response %s Errors: %s",channel,datapoint, JSON.stringify(value),error);
         callback(value);
       });
       return;
@@ -190,12 +201,12 @@ HomeMaticRPC.prototype.init = function() {
       value = String(value);
     }
 
-	debug("RPC setValue Call for %s %s Value %s Type %s",channel,datapoint,value, typeof value);
+	this.log.debug("RPC setValue Call for %s %s Value %s Type %s",channel,datapoint,value, typeof value);
 	
 	
 	
 	this.client.methodCall("setValue", [channel, datapoint, value], function(error, value) {
-	 debug("RPC setValue (%s %s) Response %s Errors: %s",channel, datapoint, JSON.stringify(value),error);
+	 this.log.debug("RPC setValue (%s %s) Response %s Errors: %s",channel, datapoint, JSON.stringify(value),error);
     });
   }
 
@@ -203,15 +214,15 @@ HomeMaticRPC.prototype.init = function() {
     var that = this;
     this.lastMessage = Math.floor((new Date()).getTime() / 1000);
     var port = this.ccuport;
-    this.log("Creating Local HTTP Client for CCU RPC Events");
+    this.log.info("Creating Local HTTP Client for CCU RPC Events");
     this.client = binrpc.createClient({
       host: this.ccuip,
       port: port,
       path: "/"
     });
-    this.log("CCU RPC Init Call on port " +  port + " for interface " + this.interface);
+    this.log.debug("CCU RPC Init Call on port %s for interface %s", port , this.interface);
     this.client.methodCall("init", ["http://" + this.localIP + ":" + this.listeningPort, "homebridge_" + this.interface], function(error, value) {
-      debug("CCU Response ...Value (%s) Error : (%s)",JSON.stringify(value) , error);
+      that.log.debug("CCU Response ...Value (%s) Error : (%s)",JSON.stringify(value) , error);
       that.lastMessage = Math.floor((new Date()).getTime() / 1000);
     });
     
@@ -228,10 +239,10 @@ HomeMaticRPC.prototype.init = function() {
 	    var now = Math.floor((new Date()).getTime() / 1000);
     	var timeDiff = now - this.lastMessage;
     	if (timeDiff > that.watchDogTimeout) {
-     		that.log("Watchdog Trigger - Reinit Connection for " + this.interface + " after idle time of " + timeDiff + " seconds");
+     		that.log.debug("Watchdog Trigger - Reinit Connection for %s after idle time of %s seconds",this.interface,timeDiff);
 		    that.lastMessage = now;
 		    that.client.methodCall("init", ["http://" + this.localIP + ":" + this.listeningPort, "homebridge_" + this.interface], function(error, value) {
-      			debug("CCU Response ...Value (%s) Error : (%s)",JSON.stringify(value) , error);
+      			that.log.debug("CCU Response ...Value (%s) Error : (%s)",JSON.stringify(value) , error);
       			that.lastMessage = Math.floor((new Date()).getTime() / 1000);
    			 });
     	}
@@ -245,11 +256,26 @@ HomeMaticRPC.prototype.init = function() {
   }
 
   HomeMaticRPC.prototype.stop = function() {
-    this.log("Removing Event Server for Interface " +this.interface);
+    this.log.info("Removing Event Server for Interface %s",this.interface);
     this.client.methodCall("init", ["http://" + this.localIP + ":" + this.listeningPort], function(error, value) {
 
     });
    }
+
+// checks if the port is in use 
+// https://gist.github.com/timoxley/1689041
+
+  HomeMaticRPC.prototype.isPortTaken = function(port, fn) {
+  var net = require('net')
+  var tester = net.createServer().once('error', function (err) {
+    if (err.code != 'EADDRINUSE') return fn(err)
+    fn(null, true)
+  })
+  .once('listening', function() {
+    tester.once('close', function() { fn(null, false) })
+    .close()
+  }).listen(port)
+ }
 
 module.exports = { 
   HomeMaticRPC : HomeMaticRPC 
