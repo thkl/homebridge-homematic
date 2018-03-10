006a67baa2cf66df2bce47098843716d5892a79c
Thomas Kluge <th.kluge@me.com>
2018-03-10 21:06:27 +0100

    worked on HmIP-BBL #197

--- ChannelServices/HomeMaticHomeKitBlindSlatServiceIP.js
+++ ChannelServices/HomeMaticHomeKitBlindSlatServiceIP.js
@@ -68,6 +68,30 @@ HomeMaticHomeKitBlindSlatServiceIP.prototype.createDeviceService = function(Serv
     this.currentStateCharacteristic["DIRECTION"] = pstate;
     pstate.eventEnabled = true;
 
+
+	this.cslat = blind.getCharacteristic(Characteristic.CurrentHorizontalTiltAngle)
+	.on('get', function(callback){
+		that.query("LEVEL_2",function(value){
+		if (callback) {
+          if (value!=undefined) {
+	          // Recalculate 
+	          let vl = -90 + (1.8 * (value*100))
+			  callback(null,vl)
+          } else {
+            callback(null,"0");
+          }
+       }
+       })
+	}.bind(this));
+
+
+	this.tslat = blind.getCharacteristic(Characteristic.TargetHorizontalTiltAngle)
+	.on('set', function(value,callback){
+		let nlv = (value+90/100)/1,8
+		that.delayed("set", "LEVEL_2", nlv, 750);      
+    })
+	}.bind(this));
+
 /**
 	Parameter DIRECTION
  0 = NONE (Standard) 
@@ -106,6 +130,13 @@ HomeMaticHomeKitBlindSlatServiceIP.prototype.datapointEvent=function(dp,newValue
 	  	that.targetPos.updateValue(value,null);
 	  })
   }
+
+  if (dp == "4:LEVEL_2") {
+	  	let vl = -90 + (1.8 * (newValue*100))
+	  	that.cslat.updateValue(vl,null);
+	  	that.tslat.updateValue(vl,null);
+  }
+
 }
 
 
