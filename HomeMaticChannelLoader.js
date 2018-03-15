531d83a2900b682de03554823051ff9cea896101
Thomas Kluge <th.kluge@me.com>
2018-03-15 21:38:46 +0100

    prepared core for #215

--- HomeMaticChannelLoader.js
+++ HomeMaticChannelLoader.js
@@ -24,14 +24,15 @@ var HomeMaticChannelLoader = function (log) {
 	var name = channel.name;
 	var adress = channel.address;
 	var intf = channel.intf;
+	this.platform = platform
 	// try to load device:type
     var serviceclass;
     var options;
     
-    var sv = this.getServiceClass(deviceType+":" + channelType);
+    var sv = this.getServiceClass(deviceType+':' + channelType);
 	serviceclass = sv['class']
-    if (sv['options'] == undefined) {
-	    options = this.getOptions(deviceType+":" + channelType);
+    if (sv['options'] == undefined) {
+	    options = this.getOptions(deviceType+':' + channelType);
 	} else {
 		options = sv['options']
 	}
@@ -41,7 +42,7 @@ var HomeMaticChannelLoader = function (log) {
       // not found try to find channeltype
       sv = this.getServiceClass(channelType);
       serviceclass = sv['class']
-	  if (sv['options'] == undefined) {
+	  if (sv['options'] == undefined) {
 	  	options = this.getOptions(channelType);
 	  } else {
 		options = sv['options']
@@ -52,7 +53,7 @@ var HomeMaticChannelLoader = function (log) {
       // not found try to find devicetype
       sv = this.getServiceClass(deviceType);
       serviceclass = sv['class']
-	  if (sv['options'] == undefined) {
+	  if (sv['options'] == undefined) {
 	  	options = this.getOptions(deviceType);
 	  } else {
 		options = sv['options']
@@ -75,19 +76,27 @@ var HomeMaticChannelLoader = function (log) {
 	      cfg = {};
       }
       
-      cfg["interface"] = channel.intf;
+      cfg['interface'] = channel.intf;
       
       // Replace Chars in name https://github.com/thkl/homebridge-homematic/issues/56
   		
       name = name.replace(/[.:#_()-]/g,' ');
-      that.log.debug("service for %s:%s is %s" , deviceType, channelType, serviceclass);
+      that.log.debug('service for %s:%s is %s' , deviceType, channelType, serviceclass);
+      
+      let cfgOptions = this.getConfigOptions(deviceType + ':' + channelType)
+      if (cfgOptions != undefined) {
+	      // Add config.json options
+	     Object.keys(cfgOptions).forEach(function (key) {
+			  cfg[key] = cfgOptions[key]
+	     })
+      }
       
 	  var accessory = new service(log, platform, id, name, channelType, adress, special, cfg, Service, Characteristic, deviceType);
 	  accessory.setReadOnly(access != 255)
 	  list.push(accessory);	
 	 }
     } else {
-      that.log.warn("There is no service for " + deviceType+":" + channelType );
+      that.log.warn('There is no service for ' + deviceType+':' + channelType );
    	}
   };
   
@@ -95,16 +104,16 @@ var HomeMaticChannelLoader = function (log) {
   HomeMaticChannelLoader.prototype.loadClass = function(serviceclass) {
   
     if (fs.existsSync(path.join(__dirname,'ChannelServices',serviceclass+'.js'))) {
-	    this.log.debug("Load BuildIn Service Class %s",serviceclass)
+	    this.log.debug('Load BuildIn Service Class %s',serviceclass)
 	    return require(path.join(__dirname,'ChannelServices',serviceclass))
     }
     
     if (fs.existsSync(path.join(this.localPath,serviceclass+'.js'))) {
-	    this.log.debug("Load Custom Service Class %s",serviceclass)
+	    this.log.debug('Load Custom Service Class %s',serviceclass)
 	    return require(path.join(this.localPath,serviceclass))
     }
 	
-	this.log.warn("No class found in %s or %s",path.join(__dirname,'ChannelServices',serviceclass+'.js'),path.join(this.localPath,serviceclass+'.js'))
+	this.log.warn('No class found in %s or %s',path.join(__dirname,'ChannelServices',serviceclass+'.js'),path.join(this.localPath,serviceclass+'.js'))
 	
 	return undefined;    
   }
@@ -113,30 +122,42 @@ var HomeMaticChannelLoader = function (log) {
   HomeMaticChannelLoader.prototype.getOptions = function(type) {
    var that = this;
    var options = undefined;
-   
-   if (this.config != undefined) {
-  		var ci = this.config["channelconfig"];
+   if (this.config != undefined) {
+  		var ci = this.config['channelconfig'];
    			ci.map(function(service) {
-   			  	if (service["type"]==type) {
-   			  	  options = service["options"];
+   			  	if (service['type']==type) {
+   			  	  options = service['options'];
 				}
 			});
    }
-   return options;
+   
+   
+     return options;
   }
 
+// load options from config.json
+
+HomeMaticChannelLoader.prototype.getConfigOptions = function(type) {
+  this.log.debug('Check config for %s',type)
+   var options = undefined;
+   let coptions = this.platform.config[type]
+   if (coptions) {
+	  options = coptions
+   }
+   return options
+ }
   
   HomeMaticChannelLoader.prototype.getServiceClass = function(type) {
    var that = this;
    var serviceclass = undefined;
    var serviceoptions = undefined;
    
-   if (this.config != undefined) {
-  		var ci = this.config["channelconfig"];
+   if (this.config != undefined) {
+  		var ci = this.config['channelconfig'];
    			ci.map(function(service) {
-   			  	if (service["type"]==type) {
-   			  	  serviceclass = service["service"]
-   			  	  serviceoptions = service["options"]
+   			  	if (service['type']==type) {
+   			  	  serviceclass = service['service']
+   			  	  serviceoptions = service['options']
 				}
 			});
    }
@@ -151,7 +172,7 @@ var HomeMaticChannelLoader = function (log) {
     var config = JSON.parse(fs.readFileSync(config_path));
     if (customServices != undefined) {
       customServices.map(function(service) {
-     	 config["channelconfig"].push(service);
+     	 config['channelconfig'].push(service);
       });
     }
     
@@ -160,7 +181,7 @@ var HomeMaticChannelLoader = function (log) {
   }
   
   catch (err) {
-   this.log.warn("Internal Channel config has errors, or was not found. You may ceck the file ChannelService/channel_config.json");
+   this.log.warn('Internal Channel config has errors, or was not found. You may ceck the file ChannelService/channel_config.json');
    throw err;
   }
   
