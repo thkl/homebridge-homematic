4037cd50e5171bc91f3207becd93b91fa7a0b378
Thomas Kluge <th.kluge@me.com>
2018-03-15 16:41:49 +0100

    renamed Servicetype for CO2 Sensor

---
+++ ChannelServices/HomeMaticHomeKitCarbonDioxideSensorService.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var HomeKitGenericService = require('./HomeKitGenericService.js').HomeKitGenericService;
+var util = require("util");
+
+
+function HomeMaticHomeKitCarbonDioxideSensorService(log,platform, id ,name, type ,adress,special, cfg, Service, Characteristic) {
+    HomeMaticHomeKitCarbonDioxideSensorService.super_.apply(this, arguments);
+}
+
+util.inherits(HomeMaticHomeKitCarbonDioxideSensorService, HomeKitGenericService);
+
+
+HomeMaticHomeKitCarbonDioxideSensorService.prototype.createDeviceService = function(Service, Characteristic) {
+
+    var that = this;
+    var co2sensor = new Service.CarbonDioxideSensor(this.name);
+	this.services.push(co2sensor);
+    
+    
+    this.co2level = co2sensor.getCharacteristic(Characteristic.CarbonDioxideDetected)
+	.on('get', function(callback) {
+      that.query("STATE",function(value){
+	      var result = Characteristic.CarbonDioxideDetected.CO2_LEVELS_ABNORMAL
+	      switch value {
+		      case 0:
+		        result = Characteristic.CarbonDioxideDetected.CO2_LEVELS_NORMAL
+		        break
+		      case 1:
+		        result = Characteristic.CarbonDioxideDetected.CO2_LEVELS_NORMAL
+		        break
+		      case 2:
+		        result = Characteristic.CarbonDioxideDetected.CO2_LEVELS_ABNORMAL
+		        break
+		  }
+       if (callback) callback(null,result);
+      });
+    }.bind(this));
+
+    this.currentStateCharacteristic["STATE"] = this.co2level;
+    this.remoteGetValue("STATE");
+
+	this.addValueMapping("STATE",0,Characteristic.CarbonDioxideDetected.CO2_LEVELS_NORMAL);
+    this.addValueMapping("STATE",1,Characteristic.CarbonDioxideDetected.CO2_LEVELS_NORMAL);
+    this.addValueMapping("STATE",2,Characteristic.CarbonDioxideDetected.CO2_LEVELS_ABNORMAL);
+    
+}
+
+
+
+module.exports = HomeMaticHomeKitCarbonDioxideSensorService; 
