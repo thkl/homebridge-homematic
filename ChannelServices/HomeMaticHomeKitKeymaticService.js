936f081cd3eb06be02afb789a1737e1a2192bd62
Thomas Kluge <th.kluge@me.com>
2018-03-16 21:00:49 +0100

    worked on #218 ...

--- ChannelServices/HomeMaticHomeKitKeymaticService.js
+++ ChannelServices/HomeMaticHomeKitKeymaticService.js
@@ -14,49 +14,50 @@ util.inherits(HomeMaticHomeKitKeymaticService, HomeKitGenericService)
 HomeMaticHomeKitKeymaticService.prototype.createDeviceService = function(Service, Characteristic) {
 
   var that = this
+  this.requerytimer;
   var door = new Service.LockMechanism(this.name)
   this.services.push(door)
 
   this.current_state = door.getCharacteristic(Characteristic.LockCurrentState)
   .on('get', function(callback) {
-    that.query('STATE',function(value){
 
-      if ((that.state['DIRECTION']!=undefined) && (that.state['DIRECTION']>0)) {
-        try {if (callback) callback(null,3)} catch (error){}
-      } else {
-        try {if (callback) callback(null,value)} catch (error){}
-      }
-
+    that.remoteGetValue('STATE',function(result){
+      let hmState = ((result=='true') || (result==true)) ? 0 : 1;
+      callback(null,hmState);
+      let parts = that.adress.split('.')
+      that.event(parts[0]+'.' +parts[1],'STATE',result)
     })
+
+
   }.bind(this))
 
-  this.currentStateCharacteristic['STATE'] = this.current_state
   this.current_state.eventEnabled = true
-
-  this.addValueMapping('STATE',1,0)
-  this.addValueMapping('STATE',0,1)
-  this.addValueMapping('STATE',false,1)
-  this.addValueMapping('STATE',true,0)
-
 
   this.target_state = door.getCharacteristic(Characteristic.LockTargetState)
 
   .on('get', function(callback) {
-    that.query('STATE',function(value){
-      try {if (callback) callback(null,value)} catch (error){}
+
+    that.remoteGetValue('STATE',function(result){
+      let hmState = ((result=='true') || (result==true)) ? 0 : 1;
+      callback(null,hmState);
+      let parts = that.adress.split('.')
+      that.event(parts[0]+'.' +parts[1],'STATE',result)
     })
+
   }.bind(this))
 
 
   .on('set', function(value, callback) {
+    clearTimeout(that.requerytimer)
     that.command('setrega','STATE' , (value==1) ? 0 : 1)
-    setTimeout(function() {
-      that.remoteGetValue('STATE')
+
+    that.requerytimer = setTimeout(function() {
+      that.queryState()
     },10000)
     callback()
   }.bind(this))
 
-  this.remoteGetValue('STATE')
+
 
   var dopener = door.addCharacteristic(Characteristic.TargetDoorState)
   .on('get', function(callback) {
@@ -74,11 +75,25 @@ HomeMaticHomeKitKeymaticService.prototype.createDeviceService = function(Service
     callback(0)
   }.bind(this))
 
-
-
-
+  this.queryState();
 }
 
+HomeMaticHomeKitKeymaticService.prototype.queryState = function(){
+  clearTimeout(this.requerytimer)
+  let that = this
+  this.remoteGetValue('STATE',function(result){
+    let parts = that.adress.split('.')
+    that.event(parts[0]+'.' +parts[1],'STATE',result)
+  })
+}
+
+HomeMaticHomeKitKeymaticService.prototype.event = function(channel,dp,newValue){
+  if (dp =='STATE') {
+    let hmState = ((newValue=='true') || (newValue==true)) ? 0 : 1;
+    this.current_state.updateValue(hmState,null)
+    this.target_state.updateValue(hmState,null)
+  }
+}
 
 
 module.exports = HomeMaticHomeKitKeymaticService
