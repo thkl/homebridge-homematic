c49434ac8c80105079d85b893464e819e19e5db1
Thomas Kluge <th.kluge@me.com>
2018-03-16 16:20:17 +0100

    should fix #215

--- ChannelServices/HomeMaticHomeKitSmokeDetectorServiceIP.js
+++ ChannelServices/HomeMaticHomeKitSmokeDetectorServiceIP.js
@@ -5,7 +5,7 @@ var util = require("util");
 
 
 function HomeMaticHomeKitSmokeDetectorServiceIP(log,platform, id ,name, type ,adress,special, cfg, Service, Characteristic) {
-    HomeMaticHomeKitSmokeDetectorServiceIP.super_.apply(this, arguments);
+  HomeMaticHomeKitSmokeDetectorServiceIP.super_.apply(this, arguments);
 }
 
 util.inherits(HomeMaticHomeKitSmokeDetectorServiceIP, HomeKitGenericService);
@@ -13,22 +13,42 @@ util.inherits(HomeMaticHomeKitSmokeDetectorServiceIP, HomeKitGenericService);
 
 HomeMaticHomeKitSmokeDetectorServiceIP.prototype.createDeviceService = function(Service, Characteristic) {
 
-    var that = this;
-    var sensor = new Service["SmokeSensor"](this.name);
-    var state = sensor.getCharacteristic(Characteristic.SmokeDetected)
-	.on('get', function(callback) {
-      that.query("SMOKE_DETECTOR_ALARM_STATUS",function(value){
-       if (callback) callback(null,value);
-      });
-    }.bind(this));
+  var that = this;
+  var sensor = new Service.SmokeSensor(this.name)
+  this.memyselfandi = this.getClazzConfigValue('single_alarm',false)
 
-    this.currentStateCharacteristic["SMOKE_DETECTOR_ALARM_STATUS"] = state;
-    state.eventEnabled = true;
-    this.services.push(sensor);
-    this.remoteGetValue("SMOKE_DETECTOR_ALARM_STATUS");
+  var state = sensor.getCharacteristic(Characteristic.SmokeDetected)
+  .on('get', function(callback) {
+    that.query("SMOKE_DETECTOR_ALARM_STATUS",function(value){
+
+      // https://github.com/thkl/homebridge-homematic/issues/215
+
+      if ((this.memyselfandi == true) && (value==1)) {
+        if (callback) callback(null,value);
+      } else {
+        if (callback) callback(null,value);
+      }
+
+    });
+  }.bind(this));
+
+  this.currentStateCharacteristic["SMOKE_DETECTOR_ALARM_STATUS"] = state;
+  state.eventEnabled = true;
+  this.services.push(sensor);
+  this.remoteGetValue("SMOKE_DETECTOR_ALARM_STATUS");
 
 }
 
+HomeMaticHomeKitSmokeDetectorServiceIP.prototype.datapointEvent=function(dp,newValue)  {
+  if (dp=='SMOKE_DETECTOR_ALARM_STATUS') {
 
+    if ((this.memyselfandi == true) && (newValue==1)) {
+      if (callback) callback(null,newValue);
+    } else {
+      if (callback) callback(null,newValue);
+    }
 
-module.exports = HomeMaticHomeKitSmokeDetectorServiceIP; 
+  }
+}
+
+module.exports = HomeMaticHomeKitSmokeDetectorServiceIP;
