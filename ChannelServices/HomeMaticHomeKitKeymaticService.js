3b49d1b1aca1f1e764fe7480cefb09603259cd92
Thomas Kluge <th.kluge@me.com>
2018-03-16 15:03:01 +0100

    catch errors

--- ChannelServices/HomeMaticHomeKitKeymaticService.js
+++ ChannelServices/HomeMaticHomeKitKeymaticService.js
@@ -1,84 +1,78 @@
-'use strict';
+'use strict'
 
-var HomeKitGenericService = require('./HomeKitGenericService.js').HomeKitGenericService;
-var util = require("util");
+var HomeKitGenericService = require('./HomeKitGenericService.js').HomeKitGenericService
+var util = require('util')
 
 
 function HomeMaticHomeKitKeymaticService(log,platform, id ,name, type ,adress,special, cfg, Service, Characteristic) {
-    HomeMaticHomeKitKeymaticService.super_.apply(this, arguments);
+  HomeMaticHomeKitKeymaticService.super_.apply(this, arguments)
 }
 
-util.inherits(HomeMaticHomeKitKeymaticService, HomeKitGenericService);
+util.inherits(HomeMaticHomeKitKeymaticService, HomeKitGenericService)
 
 
 HomeMaticHomeKitKeymaticService.prototype.createDeviceService = function(Service, Characteristic) {
 
-   var that = this;
+  var that = this
+  var door = new Service.LockMechanism(this.name)
+  this.services.push(door)
 
-    var door = new Service["LockMechanism"](this.name);
-    this.services.push(door);
+  this.current_state = door.getCharacteristic(Characteristic.LockCurrentState)
+  .on('get', function(callback) {
+    that.query('STATE',function(value){
 
-    var cstate = door.getCharacteristic(Characteristic.LockCurrentState)
-
-	.on('get', function(callback) {
-      that.query("STATE",function(value){
-       
-       if ((that.state["DIRECTION"]!=undefined) && (that.state["DIRECTION"]>0)) {
-          if (callback) callback(null,3);
-       } else {
-          if (callback) callback(null,value);
-	   }
-      
-      });
-    }.bind(this));
-
-    this.currentStateCharacteristic["STATE"] = cstate;
-    cstate.eventEnabled = true;
-
-    this.addValueMapping("STATE",1,0);
-    this.addValueMapping("STATE",0,1);
-    this.addValueMapping("STATE",false,1);
-    this.addValueMapping("STATE",true,0);
-
-
-    var tstate = door.getCharacteristic(Characteristic.LockTargetState)
-
-    .on('get', function(callback) {
-      that.query("STATE",function(value){
-       if (callback) callback(null,value);
-      });
-    }.bind(this))
-
-
-    .on('set', function(value, callback) {
-      that.command("setrega","STATE" , (value==1) ? 0 : 1)
-      setTimeout(function() {
-       that.remoteGetValue("STATE");
-      },10000);
-      callback();
-    }.bind(this));
-
-   // this.currentStateCharacteristic["STATE"] = tstate;
-   // tstate.eventEnabled = true;
-
-
-    this.remoteGetValue("STATE");
-
-    var dopener = door.addCharacteristic(Characteristic.TargetDoorState)
-    .on('get', function(callback) {
-      if (callback) callback(null,1);
-    }.bind(this))
-
-    .on('set', function(value, callback) {
-      if (value==0) {
-	      that.command("setrega","OPEN" , "true")
-    	  setTimeout(function() {
-    		dopener.setValue(1, null);
-      	  },2000);
+      if ((that.state['DIRECTION']!=undefined) && (that.state['DIRECTION']>0)) {
+        try {if (callback) callback(null,3)} catch (error){}
+      } else {
+        try {if (callback) callback(null,value)} catch (error){}
       }
-      
-      callback(0);
-    }.bind(this));
+
+    })
+  }.bind(this))
+
+  this.currentStateCharacteristic['STATE'] = this.current_state
+  this.current_state.eventEnabled = true
+
+  this.addValueMapping('STATE',1,0)
+  this.addValueMapping('STATE',0,1)
+  this.addValueMapping('STATE',false,1)
+  this.addValueMapping('STATE',true,0)
+
+
+  this.target_state = door.getCharacteristic(Characteristic.LockTargetState)
+
+  .on('get', function(callback) {
+    that.query('STATE',function(value){
+      try {if (callback) callback(null,value)} catch (error){}
+    })
+  }.bind(this))
+
+
+  .on('set', function(value, callback) {
+    that.command('setrega','STATE' , (value==1) ? 0 : 1)
+    setTimeout(function() {
+      that.remoteGetValue('STATE')
+    },10000)
+    callback()
+  }.bind(this))
+
+  this.remoteGetValue('STATE')
+
+  var dopener = door.addCharacteristic(Characteristic.TargetDoorState)
+  .on('get', function(callback) {
+    if (callback) callback(null,1)
+  }.bind(this))
+
+  .on('set', function(value, callback) {
+    if (value==0) {
+      that.command('setrega','OPEN' , 'true')
+      setTimeout(function() {
+        dopener.setValue(1, null)
+      },2000)
+    }
+
+    callback(0)
+  }.bind(this))
 
 
 
@@ -87,4 +81,4 @@ HomeMaticHomeKitKeymaticService.prototype.createDeviceService = function(Service
 
 
 
-module.exports = HomeMaticHomeKitKeymaticService; 
+module.exports = HomeMaticHomeKitKeymaticService
