3b10c9c69eeecea031e1dc76654cd30398f4c74a
Thomas Kluge <th.kluge@me.com>
2018-06-17 17:28:29 +0200

    worked on Thermostats
    Signed-off-by: Thomas Kluge <th.kluge@me.com>

---
+++ ChannelServices/HomeMaticHomeKitIPThermostatService.js
@@ -0,0 +1,157 @@
+'use strict';
+
+var HomeKitGenericService = require('./HomeKitGenericService.js').HomeKitGenericService;
+var util = require("util");
+var moment = require('moment');
+
+function HomeMaticHomeKitIPThermostatService(log,platform, id ,name, type ,adress,special, cfg, Service, Characteristic) {
+    HomeMaticHomeKitIPThermostatService.super_.apply(this, arguments);
+}
+
+util.inherits(HomeMaticHomeKitIPThermostatService, HomeKitGenericService);
+
+
+HomeMaticHomeKitIPThermostatService.prototype.createDeviceService = function(Service, Characteristic) {
+
+	var that = this;
+    this.usecache = false;
+ 	var FakeGatoHistoryService = require('./fakegato-history.js')(this.platform.homebridge);
+ 	var thermo = new Service["Thermostat"](this.name);
+    this.services.push(thermo);
+	this.log.debug("Adding Log Service for %s",this.displayName);
+	this.loggingService = new FakeGatoHistoryService("thermo", this, {storage: 'fs', path: this.platform.localCache,disableTimer:true});
+	this.loggingService.adress = this.adress;
+	this.services.push(this.loggingService);
+
+
+
+    var mode = thermo.getCharacteristic(Characteristic.CurrentHeatingCoolingState)
+    .on('get', function(callback) {
+
+      this.query("SET_POINT_TEMPERATURE",function(value) {
+         if (value<6.0){
+           that.currentStateCharacteristic["CONTROL_MODE"].setValue(1, null);
+           if (callback) callback(null,0);
+         } else {
+           that.currentStateCharacteristic["CONTROL_MODE"].setValue(0, null);
+           if (callback) callback(null,1);
+         }
+      });
+
+
+    }.bind(this));
+
+    this.currentStateCharacteristic["CONTROL_MODE"] = mode;
+    mode.eventEnabled = true;
+
+    var targetMode = thermo.getCharacteristic(Characteristic.TargetHeatingCoolingState)
+    .on('get', function(callback) {
+
+      this.query("SET_POINT_TEMPERATURE",function(value) {
+         if (value<6.0){
+          if (callback) callback(null,0);
+         } else {
+          if (callback) callback(null,1);
+         }
+      });
+
+    }.bind(this))
+
+    .on('set', function(value, callback) {
+      if (value==0) {
+        this.command("setrega", "SET_POINT_TEMPERATURE", 0);
+        this.cleanVirtualDevice("SET_POINT_TEMPERATURE");
+      } else {
+        this.cleanVirtualDevice("SET_POINT_TEMPERATURE");
+      }
+      callback();
+    }.bind(this));
+
+    targetMode.setProps({
+        format: Characteristic.Formats.UINT8,
+        perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY],
+       maxValue: 1,
+       minValue: 0,
+       minStep: 1,
+    });
+
+   var cctemp = thermo.getCharacteristic(Characteristic.CurrentTemperature)
+   .setProps({ minValue: -100 })
+    .on('get', function(callback) {
+      this.remoteGetValue("ACTUAL_TEMPERATURE",function(value){
+       if (callback) callback(null,value);
+      });
+    }.bind(this));
+
+    this.currentStateCharacteristic["ACTUAL_TEMPERATURE"] = cctemp;
+    cctemp.eventEnabled = true;
+
+
+   var cchum = thermo.getCharacteristic(Characteristic.CurrentRelativeHumidity)
+   .on('get', function(callback) {
+      this.remoteGetValue("HUMIDITY",function(value){
+       if (callback) callback(null,value);
+      });
+    }.bind(this));
+
+    this.currentStateCharacteristic["HUMIDITY"] = cchum;
+    cchum.eventEnabled = true;
+
+
+
+    var ttemp = thermo.getCharacteristic(Characteristic.TargetTemperature)
+    .setProps({ minValue: 6.0, maxValue: 30.5, minStep: 0.1 })
+	.on('get', function(callback) {
+
+      this.query("SET_POINT_TEMPERATURE",function(value) {
+
+      if (value<6) {
+         value=6;
+      }
+	  if (value>30) {
+         value=30.5;
+      }
+         if (callback) callback(null,value);
+      });
+
+    }.bind(this))
+
+    .on('set', function(value, callback) {
+      	this.delayed("setrega", "SET_POINT_TEMPERATURE", value,500);
+	    callback();
+    }.bind(this));
+
+    this.currentStateCharacteristic["SET_POINT_TEMPERATURE"] = ttemp;
+    ttemp.eventEnabled = true;
+
+    thermo.getCharacteristic(Characteristic.TemperatureDisplayUnits)
+    .on('get', function(callback) {
+      if (callback) callback(null, Characteristic.TemperatureDisplayUnits.CELSIUS);
+    }.bind(this));
+
+
+   this.remoteGetValue("ACTUAL_TEMPERATURE");
+   this.remoteGetValue("HUMIDITY");
+   this.remoteGetValue("SET_POINT_TEMPERATURE");
+
+
+
+}
+
+HomeMaticHomeKitIPThermostatService.prototype.datapointEvent= function(dp,newValue) {
+
+	if (dp=='ACTUAL_TEMPERATURE') {
+		this.loggingService.addEntry({time: moment().unix(), currentTemp:parseFloat(newValue)});
+	}
+
+	if (dp=='HUMIDITY') {
+		this.loggingService.addEntry({time: moment().unix(), humidity:parseFloat(newValue)});
+	}
+
+	if (dp=='SET_POINT_TEMPERATURE') {
+		this.loggingService.addEntry({time: moment().unix(), setTemp:parseFloat(newValue)});
+	}
+
+}
+
+module.exports = HomeMaticHomeKitIPThermostatService;
---
+++ ChannelServices/HomeMaticHomeKitThermometerService0.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var HomeKitGenericService = require('./HomeKitGenericService.js').HomeKitGenericService;
+var util = require("util");
+var moment = require('moment');
+
+
+function HomeMaticHomeKitThermometerService(log,platform, id ,name, type ,adress,special, cfg, Service, Characteristic) {
+  HomeMaticHomeKitThermometerService.super_.apply(this, arguments);
+}
+
+util.inherits(HomeMaticHomeKitThermometerService, HomeKitGenericService);
+
+
+HomeMaticHomeKitThermometerService.prototype.createDeviceService = function(Service, Characteristic) {
+
+  var that = this;
+  this.usecache = false;
+  var thermo = new Service.TemperatureSensor(this.name);
+  this.services.push(thermo);
+  // Enable log
+  this.enableLoggingService("thermo");
+
+
+  this.cctemp = thermo.getCharacteristic(Characteristic.CurrentTemperature)
+  .setProps({ minValue: -100 })
+  .on('get', function(callback) {
+    this.remoteGetValue("TEMPERATURE",function(value){
+      that.addLogEntry({currentTemp:parseFloat(value)})
+      if (callback) callback(null,value);
+    });
+  }.bind(this));
+
+  this.setCurrentStateCharacteristic(this.channelnumber + ":TEMPERATURE",this.cctemp);
+  this.eventEnabled = true;
+
+  this.queryData();
+}
+
+HomeMaticHomeKitThermometerService.prototype.queryData = function() {
+  var that = this;
+  this.query("TEMPERATURE",function(value){
+    that.addLogEntry({currentTemp:parseFloat(value)})
+    //create timer to query device every 10 minutes
+    that.refreshTimer = setTimeout(function(){that.queryData()}, 10 * 60 * 1000);
+  })
+}
+
+HomeMaticHomeKitThermometerService.prototype.shutdown = function() {
+  clearTimeout(this.refreshTimer)
+}
+
+HomeMaticHomeKitThermometerService.prototype.datapointEvent= function(dp,newValue) {
+  if (this.isDataPointEvent(dp,'TEMPERATURE')) {
+    this.cctemp.updateValue(newValue,null)
+    this.addLogEntry({currentTemp:parseFloat(newValue)});
+  }
+}
+
+module.exports = HomeMaticHomeKitThermometerService;
