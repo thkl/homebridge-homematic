04b9e25318e9ff93f8ea8e88fbf1e8b54da24076
Thomas Kluge <th.kluge@me.com>
2018-11-03 10:28:39 +0100

    fix for firmware > 3.41.7

--- HomeMaticRPC.js
+++ HomeMaticRPC.js
@@ -37,27 +37,28 @@ var HomeMaticRPC = function (log, ccuip, port, system, platform) {
       this.interface = 'BidCos-RF.'
       this.ccuport = 2001
 
-      if (semver.lt(process.version, '4.5.0')) {
-        this.log.info('using xmprpc for communication with BidCos-RF')
-        this.rpc = xmlrpc
-        this.rpcInit = 'http://'
-      } else {
-        this.log.info('using binrpc for communication with BidCos-RF')
-        this.rpc = binrpc
-        this.rpcInit = 'xmlrpc_bin://'
-      }
+      //if (semver.lt(process.version, '4.5.0')) {
+      this.log.info('using xmprpc for communication with BidCos-RF')
+      this.rpc = xmlrpc
+      this.rpcInit = 'http://'
+      //} else {
+      //  this.log.info('using binrpc for communication with BidCos-RF')
+      //  this.rpc = binrpc
+      //  this.rpcInit = 'xmlrpc_bin://'
+      //}
 
       break
 
     case 1 :
       this.interface = 'BidCos-Wired.'
-      if (semver.lt(process.version, '4.5.0')) {
-        this.rpc = xmlrpc
-        this.rpcInit = 'http://'
-      } else {
-        this.rpc = binrpc
-        this.rpcInit = 'xmlrpc_bin://'
-      }
+      //if (semver.lt(process.version, '4.5.0')) {
+      this.log.info('using xmprpc for communication with BidCos-Wired')
+      this.rpc = xmlrpc
+      this.rpcInit = 'http://'
+      //} else {
+      //  this.rpc = binrpc
+      //  this.rpcInit = 'xmlrpc_bin://'
+      //}
       this.ccuport = 2000
       break
 
