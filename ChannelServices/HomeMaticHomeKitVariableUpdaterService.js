20c0123b7d19baffe34b7a09da7ab66124443f01
Thomas Kluge <th.kluge@me.com>
2019-01-30 20:19:26 +0100

    worked on variable updater

--- ChannelServices/HomeMaticHomeKitVariableUpdaterService.js
+++ ChannelServices/HomeMaticHomeKitVariableUpdaterService.js
@@ -15,30 +15,40 @@ HomeMaticHomeKitVariableUpdaterService.prototype.createDeviceService = function 
   var cc = vup.getCharacteristic(Characteristic.ProgrammableSwitchEvent)
     .on('set', function (value, callback) {
       // triggerd twice so only react on value = 1
-      if (value === 1) {
-        // Special is a list of my Variables so create a Rega Request
-        var script = ''
-        this.special.map(function (variable) {
-          script = script + "WriteLine('" + variable + "(---)'#dom.GetObject('" + variable + "').State());"
-        })
-
-        that.command('sendregacommand', '', script, function (result) {
-          // Parse result an set all Variables
-          result.split('\r\n').map(function (tmpvar) {
-            var vn = tmpvar.split('(---)')[0]
-            var vv = tmpvar.split('(---)')[1]
-            if ((vn !== undefined) && (vv !== undefined)) {
-              that.platform.remoteSetValue(vn, 'STATE', vv)
-            }
-          })
-        })
-        if (callback) callback(null, value)
+      if ((value === 1) || (value === true)) {
+        that.updateVariables()
       }
-    }.bind(this))
+      if (callback) callback(null, value)
+    })
 
   this.currentStateCharacteristic['PRESS_SHORT'] = cc
   cc.eventEnabled = true
   this.services.push(vup)
+}
+
+HomeMaticHomeKitVariableUpdaterService.prototype.updateVariables = function () {
+  // Special is a list of my Variables so create a Rega Request
+  let that = this
+  var script = ''
+  this.special.map(function (variable) {
+    script = script + "WriteLine('" + variable + "(---)'#dom.GetObject('" + variable + "').State());"
+  })
+  that.command('sendregacommand', '', script, function (result) {
+    // Parse result an set all Variables
+    result.split('\r\n').map(function (tmpvar) {
+      var vn = tmpvar.split('(---)')[0]
+      var vv = tmpvar.split('(---)')[1]
+      if ((vn !== undefined) && (vv !== undefined)) {
+        that.log.debug('Update variable %s with %s', vn, vv)
+        that.platform.remoteSetValue(vn, 'STATE', vv)
+      }
+    })
+  })
+}
+
+HomeMaticHomeKitVariableUpdaterService.prototype.datapointEvent = function (dp, newValue) {
+  this.log.debug('Variable update trigger was pressed (%s)', newValue)
+  this.updateVariables()
 }
 
 module.exports = HomeMaticHomeKitVariableUpdaterService
