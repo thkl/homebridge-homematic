4b5dca5f262db65488ca2af54f8270475e956c45
Thomas Kluge <th.kluge@me.com>
2020-02-05 19:34:03 +0100

    change for #524

--- ChannelServices/HomeMaticHomeKitLeakSensorService.js
+++ ChannelServices/HomeMaticHomeKitLeakSensorService.js
@@ -12,21 +12,40 @@ util.inherits(HomeMaticHomeKitLeakSensorService, HomeKitGenericService)
 HomeMaticHomeKitLeakSensorService.prototype.createDeviceService = function (Service, Characteristic) {
   var that = this
   var leakSensor = new Service['LeakSensor'](this.name)
-  var state = leakSensor.getCharacteristic(Characteristic.LeakDetected)
+  this.state = leakSensor.getCharacteristic(Characteristic.LeakDetected)
     .on('get', function (callback) {
       that.query('STATE', function (value) {
         if (callback) callback(null, value)
       })
     })
 
-  this.addValueMapping('STATE', 0, 0)
-  this.addValueMapping('STATE', 1, 1)
-  this.addValueMapping('STATE', 2, 1)
-
-  this.currentStateCharacteristic['STATE'] = state
-  state.eventEnabled = true
+  this.currentStateCharacteristic['STATE'] = this.state
+  this.state.eventEnabled = true
   this.services.push(leakSensor)
   this.remoteGetValue('STATE')
+}
+
+HomeMaticHomeKitLeakSensorService.prototype.datapointEvent = function (dp, newValue) {
+  this.log.info('[Leak Service] Event %s vs %s value is %s', dp, this.channelnumber + '.STATE', newValue)
+  if (dp === this.channelnumber + '.STATE') {
+    switch (newValue) {
+      case 0:
+        this.state.updateValue(0, null)
+        break
+      case false:
+        this.state.updateValue(0, null)
+        break
+      case true:
+        this.state.updateValue(1, null)
+        break
+      case 1:
+        this.state.updateValue(1, null)
+        break
+      case 2:
+        this.state.updateValue(1, null)
+        break
+    }
+  }
 }
 
 module.exports = HomeMaticHomeKitLeakSensorService
